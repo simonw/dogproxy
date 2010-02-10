df8a57ff5ed87592c25dd17623282b1f999427ae
Michael Stillwell <mjs@beebo.org>
2010-02-10 01:05:17 +0000

    Store queue of waiting responses as event listeners, rather than "manually" manipulating the in_flight object.

--- dogproxy.js
+++ dogproxy.js
@@ -4,10 +4,11 @@ var port = 8009;
 var sys = require('sys'), 
     http = require('http'),
     url_mod = require('url'),
+    events = require('events'),
     rest = require('./lib/restler')
 ;
 
-var in_flight = {};
+var in_flight = new events.EventEmitter();
 
 http.createServer(function (req, res) {
     var bits = url_mod.parse(req.url, true);
@@ -24,26 +25,21 @@ http.createServer(function (req, res) {
         res.sendBody(body);
         res.finish();
     }
-    if (in_flight[url]) {
-        in_flight[url].push(complete);
+    if (in_flight.listeners(url).length > 0) {
+        in_flight.addListener(url, complete);
         sys.puts('... already in flight, adding to queue')
     } else {
-        in_flight[url] = [complete];
+        in_flight.addListener(url, complete);
         sys.puts('... kicking off backend HTTP fetch')
         function done(body, response) {
             var status = response.statusCode;
             var content_type = response['content-type'];
-            if (!in_flight[url]) {
-                return;
-            }
             sys.puts(
-                'Fetched ' + url + ', alerting ' + in_flight[url].length + 
+                'Fetched ' + url + ', alerting ' + in_flight.listeners(url).length + 
                 ' waiting clients'
             );
-            in_flight[url].forEach(function(callback) {
-                callback(status, content_type, body);
-            });
-            delete in_flight[url];
+            in_flight.emit(url, status, content_type, body);
+            in_flight.listeners(url).length = 0;
         }
         rest.get(url).addListener('complete',done).addListener('error',done);
     }
